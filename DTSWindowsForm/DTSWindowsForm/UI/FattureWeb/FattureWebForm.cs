9f0a1876eda050b7d2b62ef85b41634abd165ba9
Lucas Veríssimo Leite <lucass.verissimo@gmail.com>
2025-03-05 10:55:43 -0300

    removendo limitação de buscar somente 1000 faturas.

--- DTSWindowsForm/DTSWindowsForm/UI/FattureWeb/FattureWebForm.cs
+++ DTSWindowsForm/DTSWindowsForm/UI/FattureWeb/FattureWebForm.cs
@@ -353,7 +353,6 @@ namespace DTSWindowsForm.UI.FattureWeb
                     {
                         hasMoreData = false;
                     }
-                    hasMoreData = false;
                 }
                 catch
                 {
